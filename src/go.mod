b57808422542fe5706d706c7b843d2017267d351
liwei <liwei901009@163.com>
2024-08-07 00:24:23 +0800

    引入 tidb lex yacc sql 解析模块，并写测试用例

--- src/go.mod
+++ src/go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/liwei330249526/cckv v1.0.2
+	github.com/pingcap/parser v0.0.0-20200623164729-3a18f1e5dceb
 	github.com/smartystreets/goconvey v1.6.4
 )
 
--- src/sql/rparser/yaccParser_test.go
+++ src/sql/rparser/yaccParser_test.go
@@ -1,18 +1,53 @@
 package rparser
 
 import (
-	"course/parser"
 	"fmt"
+	"github.com/pingcap/parser"
+	"github.com/pingcap/parser/ast"
+	_ "github.com/pingcap/parser/test_driver"
 	"testing"
 )
 
 func TestYacc(t *testing.T) {
-	sql := "select * from table t1;"
+	sql := "select a,b from t1;"
 	p := parser.New()
 	stmtNodes, _, err := p.Parse(sql, "", "")
 	if err != nil {
 		return
 	}
 	fmt.Println(stmtNodes)
+}
 
+type colX struct{
+	colNames []string
+}
+
+func (v *colX) Enter(in ast.Node) (ast.Node, bool) {
+	fmt.Printf("%T\n", in)
+	if name, ok := in.(*ast.ColumnName); ok {
+		v.colNames = append(v.colNames, name.Name.O)
+	}
+	return in, false
+}
+
+func (v *colX) Leave(in ast.Node) (ast.Node, bool) {
+	return in, true
+}
+
+func extract(rootNode *ast.StmtNode) []string {
+	v := &colX{}
+	(*rootNode).Accept(v)
+	return v.colNames
+}
+func TestYacc2(t *testing.T) {
+	sql := "select a,b from t1;"
+	p := parser.New()
+	stmtNodes, _, err := p.Parse(sql, "", "")
+	if err != nil {
+		fmt.Printf("parse error: %v\n", err.Error())
+		return
+	}
+	fmt.Printf("%v\n", extract(&stmtNodes[0]))
+
+	fmt.Println("end")
 }
